6da2035472c19b92d3d0cdb1e720da313fbeaa94
SoinSoin <vincent.retore@gmail.com>
2018-04-05 11:12:55 +0200

    ma navbar

---
+++ app/dist/js/navfoot.js
@@ -0,0 +1,14 @@
+Vue.component('navmenu',{
+template:'<div id="containerElnav"><img src=""><div id="menuptgauche"><img src=""></div><div id="menuptdroit"><a>test</a> <a>test</a> <a>test</a>  <a>test</a>  <a>test</a>  <a>test</a> </div><img src=""></div> '
+})
+new Vue({
+    el:'nav',
+    })
+
+    Vue.component('imgheader',{
+        template:'<div id="containerheader"><img src="">test </div> '
+        })
+        new Vue({
+            el:'header',
+            })
+
