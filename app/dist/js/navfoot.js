4b3a1dea85d9c4e1a956dc2ab75bc3677a11f3c2
SoinSoin <vincent.retore@gmail.com>
2018-04-06 10:46:49 +0200

    pas mal d'jout

--- app/dist/js/navfoot.js
+++ app/dist/js/navfoot.js
@@ -1,14 +1,37 @@
-Vue.component('navmenu',{
-template:'<div id="containerElnav"><img src=""><div id="menuptgauche"><img src=""></div><div id="menuptdroit"><a>test</a> <a>test</a> <a>test</a>  <a>test</a>  <a>test</a>  <a>test</a> </div><img src=""></div> '
-})
+Vue.component("navmenu", {
+  template: '<div id="containerElnav"><div id="menuptgauche"><img  src="dist/logocolo.jpg" class="ImgLogo"></div><div id="menuptdroit"> <div class="containerBtnTitle"><a href="" class="">Accueil</a><div class="petitsAngre"><i class="fas fa-cog petitAngre1"></i><i class="fas fa-cog petitAngre2"></i> </div></div><div class="containerBtnTitle"><a href="" class="">Agenda</a><div class="petitsAngre"><i class="fas fa-cog petitAngre1"></i><i class="fas fa-cog petitAngre2"></i> </div></div><div class="containerBtnTitle"><a href="" class="">A propos</a><div class="petitsAngre"><i class="fas fa-cog petitAngre1"></i><i class="fas fa-cog petitAngre2"></i> </div></div><div class="containerBtnTitle"><a href="" class="">Contact</a><div class="petitsAngre"><i class="fas fa-cog petitAngre1"></i><i class="fas fa-cog petitAngre2"></i> </div></div><div class="containerBtnTitle"><a href="" class="">Commentaire</a><div class="petitsAngre"><i class="fas fa-cog petitAngre1"></i><i class="fas fa-cog petitAngre2"></i> </div></div><div class="containerBtnTitle"><a href="" class="">Galerie</a><div class="petitsAngre"><i class="fas fa-cog petitAngre1"></i><i class="fas fa-cog petitAngre2"></i> </div></div><div class="containerBtnTitle"><a href="" class="">Pro-presse</a><div class="petitsAngre"><i class="fas fa-cog petitAngre1"></i><i class="fas fa-cog petitAngre2"></i> </div></div> </div><div class="imgAngre"><i class="fas fa-cog GrosAngre"></i></div></div>'
+});
 new Vue({
-    el:'nav',
-    })
+  el: "nav"
+});
 
-    Vue.component('imgheader',{
-        template:'<div id="containerheader"><img src="">test </div> '
-        })
-        new Vue({
-            el:'header',
-            })
+$(document).ready(function () {
+  function respNav() {
+    var laptop = window.matchMedia("(max-width : 860px)");
+    if (laptop.matches) {
+      $("#menuptdroit").css({
+        display: "none"
+      });
+      var clickBtn = false;
+      $(".GrosAngre").on("click", function () {
+        if (clickBtn) {
+          $("#menuptdroit").slideUp();
+          clickBtn = false;
+        } else {
+          $("#menuptdroit").slideDown();
+          clickBtn = true;
+        }
+      }) ;
+    }
+    if (laptop.matches == false) {
+      clickBtn = false;
+      $(".GrosAngre").off();
+     $("#menuptdroit").css({
+        display: "flex"
+      });
 
+    }
+    window.addEventListener("resize", respNav, true);
+  }
+  respNav();
+});
