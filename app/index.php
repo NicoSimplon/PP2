4fd2f78f3f6784b1b7b09585647b98e02c97c656
SoinSoin <vincent.retore@gmail.com>
2018-04-06 10:50:10 +0200

    modif

--- app/index.php
+++ app/index.php
@@ -1,25 +1,45 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
+
 <head>
-	<title>Accueil</title>
-	<meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="dist/css/semantic.min.css">
-    <link rel="stylesheet" type="text/css" href="dist/css/vuetify.css">
-    <link rel="stylesheet" type="text/css" href="dist/css/main.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css" integrity="sha384-v2Tw72dyUXeU3y4aM2Y0tBJQkGfplr39mxZqlTBDUZAb9BGoC40+rdFCG0m10lXk"
+    crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/fontawesome.css" integrity="sha384-q3jl8XQu1OpdLgGFvNRnPdj5VIlCvgsDQTQB6owSOHWlAurxul7f+JpUOVdAiJ5P"
+    crossorigin="anonymous">    
+    <link rel="stylesheet" type="text/css" href="dist/css/navfoot.css"></link>
 </head>
+
 <body>
-	<nav id="barnave">
-		<navigation></navigation>
-        <!-- <div v-for="nav in navi">{{nav.lien}}</div> -->
-	</nav>
-	
-	<script src="dist/js/vue.js"></script>
-    <script src="dist/js/jquery.min.js"></script>
-    <script src="dist/js/vuetify.js"></script>
-    <script src="dist/js/semantic.min.js"></script>
-    <script src="dist/js/main.js"></script>
+    <header>
+    <p>une image</p>
+    </header>
+    <nav>
+        <navmenu></navmenu>
+    </nav>
+
+    <section>
+
+    </section>
+
+
+    <section>
+
+    </section>
+
+    <footer></footer>
+
+    <script src="dist/js/vue.js"></script>
+    <script src="dist/js/jquery.js"></script>
+    <script src="dist/js/swiper.js"></script>
+    <script src="dist/js/navfoot.js"></script>
 </body>
+
 </html>
+
+
+
 
