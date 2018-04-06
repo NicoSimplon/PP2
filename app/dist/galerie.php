74283fc561d09012e6cb98b6486e1959c931d7c9
NicoSimplon <marty.nicolas11@laposte.net>
2018-04-06 16:42:42 +0200

    galerie pas fonctionnelle

---
+++ app/dist/galerie.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Galerie</title>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css" integrity="sha384-v2Tw72dyUXeU3y4aM2Y0tBJQkGfplr39mxZqlTBDUZAb9BGoC40+rdFCG0m10lXk"
+    crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/fontawesome.css" integrity="sha384-q3jl8XQu1OpdLgGFvNRnPdj5VIlCvgsDQTQB6owSOHWlAurxul7f+JpUOVdAiJ5P"
+    crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/navfoot.css"></link>
+    <link rel="stylesheet" type="text/css" href="css/materialize.css"> 
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<!-- Navbar -->
+		
+	<nav>
+	    <navmenu></navmenu>
+	</nav>
+
+	<!-- Galerie -->
+	<h1>Galerie</h1>
+	<br>
+	<div class="row">
+
+		<div class="col m2 hide-on-small-and-down"></div>
+
+		<div class="input-field col s12 m8">
+		    <select>
+			    <option value="" disabled selected>Choose your option</option>
+			    <option value="1">Option 1</option>
+			    <option value="2">Option 2</option>
+			    <option value="3">Option 3</option>
+		    </select>
+		    <label>Sélectionner l'événement</label>
+
+			<div class="row">
+				<div class="col s12 m4 card blue-grey darken-1">
+					<img class="materialboxed" width="100%" src="logo/logocolo.jpg">
+				</div>
+				<div class="col s12 m4 card blue-grey darken-1">
+					<img class="materialboxed" width="100%" src="logo/logocolo.jpg">
+				</div>
+				<div class="col s12 m4 card blue-grey darken-1">
+					<img class="materialboxed" width="100%" src="logo/logocolo.jpg">
+				</div>
+				<div class="col s12 m4 card blue-grey darken-1">
+					<img class="materialboxed" width="100%" src="logo/logocolo.jpg">
+				</div>
+				<div class="col s12 m4 card blue-grey darken-1">
+					<img class="materialboxed" width="100%" src="logo/logocolo.jpg">
+				</div>
+				<div class="col s12 m4 card blue-grey darken-1">
+					<img class="materialboxed" width="100%" src="logo/logocolo.jpg">
+				</div>
+				<div class="col s12 m4 card blue-grey darken-1">
+					<img class="materialboxed" width="100%" src="logo/logocolo.jpg">
+				</div>
+				<div class="col s12 m4 card blue-grey darken-1">
+					<img class="materialboxed" width="100%" src="logo/logocolo.jpg">
+				</div>
+				<div class="col s12 m4 card blue-grey darken-1">
+					<img class="materialboxed" width="100%" src="logo/logocolo.jpg">
+				</div>
+			</div>
+
+		</div>
+
+		<div class="col m2 hide-on-small-and-down"></div>
+	</div>
+
+	<!-- Footer -->
+
+	<script src="js/jquery.js"></script>
+	<script src="js/vue.js"></script>
+	<script src="js/materialize.js"></script>
+	<script src="js/navfoot.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
