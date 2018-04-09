51fc2a375d8a7e26b99fee8f50f53e1686578182
SoinSoin <vincent.retore@gmail.com>
2018-04-09 17:20:29 +0200

    creation du template dela carte

---
+++ app/dist/evenement.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/materialize.css"></link>
+    <link rel="stylesheet" type="text/css" href="css/main.css"></link>
+    <link rel="stylesheet" type="text/css" href="css/navfoot.css"></link>
+
+
+</head>
+<i class="fab fa-facebook"></i>
+<i class="fab fa-facebook-square"></i>
+<body>
+    <nav>
+        <navmenu></navmenu>
+    </nav>
+
+
+    <div class="containerCenterWrap">
+
+        <div class="card sizeCard">
+            <div class="headerCard">
+                <span class="TitleDate card-title grey-text text-darken-4">Card Title </span>
+            </div>
+            <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator cardImg" src="logocolo.jpg">
+            </div>
+            <div class="card-content ResumeCard">
+                <div class="containerInfosView">
+                    <p class="NameEvent">Evenements</p>
+                </div>
+                <div class="containerInfosView">
+                    <p class="InfosView">Heure</p>
+                </div>
+                <div class="footerCard">
+                    <a class="plus activator">En Savoir Plus</a>
+                </div>
+            </div>
+            <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">Card Title
+                    <i class="material-icons right">close</i>
+                </span>
+                <p></p>
+            </div>
+        </div>
+
+
+
+
+
+
+
+    </div>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+    <script src="js/vue.js"></script>
+    <script src="js/jquery.js"></script>
+    <script src="js/navfoot.js"></script>
+    <script src="js/materialize.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/swipe.js"></script>
+</body>
+
+</html>
--- app/dist/js/main.js
+++ app/dist/js/main.js
@@ -1,17 +1,12 @@
-Vue.component('navigation', {
-	props: ['navi'],
-	template: '<a href="index.php">Accueil</a><a href="evenement.php">Agenda</a><a href="galerie.php">Galerie</a><a href="commentaire.php">Livre d\'or</a><a href="qui_sommes_nous.php">Qui sommes nous?</a><a href="pro_presse.php">Pro / Presse</a><a href="contact.php">Contact</a>'
-})
+$(document).ready(function(){
 
-// var navbarre = new Vue({
-// 	el: '#barnave',
-// 	data: {
-// 		navi:[{lien: '<a href="index.php">Accueil</a>'},
-// 			{lien: '<a href="evenement.php">Agenda</a>'},
-// 			{lien: '<a href="galerie.php">Galerie</a>'},
-// 			{lien: '<a href="commentaire.php">Livre d\'or</a>'},
-// 			{lien: '<a href="qui_sommes_nous.php">Qui sommes nous?</a>'},
-// 			{lien: '<a href="pro_presse.php">Pro / Presse</a>'},
-// 			{lien: '<a href="contact.php">Contact</a>'}]
-// 	}
-// })
+	// $(".card") .mouseout(function() {
+	// 	$(".card-reveal ", this).css({display: "none", transform: "translateY(0)"});
+	// 	$(".card",this).css({overflow: "hidden"});
+		
+	//   })
+	//   .mouseover(function() {
+	// 	$(" .card-reveal ", this).css({display: "block", transform: "translateY(-100%)",cursor:"pointer"});
+	// 	$(".card",this).css({overflow: "visible"});
+	//   });
+});
