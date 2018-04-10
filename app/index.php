4e5bb4ef0d5b031b991d92348895902923533b27
NicoSimplon <marty.nicolas11@laposte.net>
2018-04-10 10:39:31 +0200

    préparation merge

--- app/index.php
+++ app/index.php
@@ -99,15 +99,7 @@
 		<!-- Commentaires Facebook -->
 			<div>
 				<img src="dist/img/fb.jpg" width="15%">
-				<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.12';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-<div class="fb-comments" data-href="https://www.facebook.com/colo.co.81/" data-numposts="3"></div>
+				
 				
 			</div>
 			
