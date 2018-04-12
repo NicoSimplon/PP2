bf08c46850de546dae564530d34ebf1ce476a95b
SoinSoin <vincent.retore@gmail.com>
2018-04-12 21:48:48 +0200

    voila

--- app/index.php
+++ app/index.php
@@ -8,9 +8,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css" integrity="sha384-v2Tw72dyUXeU3y4aM2Y0tBJQkGfplr39mxZqlTBDUZAb9BGoC40+rdFCG0m10lXk"
-	  crossorigin="anonymous">
+	    crossorigin="anonymous">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/fontawesome.css" integrity="sha384-q3jl8XQu1OpdLgGFvNRnPdj5VIlCvgsDQTQB6owSOHWlAurxul7f+JpUOVdAiJ5P"
-	  crossorigin="anonymous">
+	    crossorigin="anonymous">
 	<!-- <link rel="stylesheet" type="text/css" href="dist/css/swiper.css"></link> -->
 	<link rel="stylesheet" type="text/css" href="dist/css/navfoot.css"></link>
 	<link rel="stylesheet" type="text/css" href="dist/css/materialize.css">
@@ -22,9 +22,9 @@
 <body>
 	<!-- Partie contenant le bandeau -->
 
-	<!-- <img class="bandeau" src="dist/img/bandeau.jpg" width="100%" height="auto"> -->
+	<img class="bandeau" src="dist/img" width="100%" height="auto">
 
-	<!-- Partie contenant la navbar -->
+
 
 	<nav>
 		<navmenu></navmenu>
@@ -34,43 +34,51 @@
 
 	<section>
 
-			<div class="carousel carousel-slider center">
-					<div class="carousel-fixed-item center">
-						<a class="btn waves-effect white grey-text darken-text-2">button</a>
-					</div>
-					<div class="carousel-item red white-text" href="#one!">
-						<h2>First Panel</h2>
-						<p class="white-text">This is your first panel</p>
-					</div>
-					<div class="carousel-item amber white-text" href="#two!">
-						<h2>Second Panel</h2>
-						<p class="white-text">This is your second panel</p>
-					</div>
-					<div class="carousel-item green white-text" href="#three!">
-						<h2>Third Panel</h2>
-						<p class="white-text">This is your third panel</p>
-					</div>
-					<div class="carousel-item blue white-text" href="#four!">
-						<h2>Fourth Panel</h2>
-						<p class="white-text">This is your fourth panel</p>
-					</div>
+		<div class="carousel carousel-slider  size">
+			<div class="carousel-item red white-text" href="#one!">
+				<div class="styletext">
+					<h2 class="infoActua">First Panel</h2>
+					<p class="white-text infoActua">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam sit fugiat eos ipsam veniam, aliquam perspiciatis
+						voluptatem consectetur expedita, cupiditate dignissimos beatae architecto fuga id molestiae nulla at? Accusamus, sed!</p>
 				</div>
-
+				<img src="concert.png " class="testImg">
+			</div>
+			<div class="carousel-item amber white-text" href="#two!">
+				<div class="styletext">
+					<h2 class="infoActua">Second Panel</h2>
+					<p class="white-text infoActua">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde eum necessitatibus eius, accusamus ratione beatae ipsam
+						corrupti quia tempore nemo minima inventore omnis quaerat iure, harum neque nam amet iusto.</p>
+				</div>
+			</div>
+			<div class="carousel-item green white-text" href="#three!">
+				<div class="styletext">
+					<h2 class="infoActua">Third Panel</h2>
+					<p class="white-text infoActua">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint iure, voluptas sed ea culpa quisquam cupiditate. Possimus
+						aperiam non voluptatibus! Corporis error nesciunt autem dolore accusantium aliquid deleniti quibusdam libero.</p>
+				</div>
+			</div>
+			<div class="carousel-item blue white-text" href="#four!">
+				<div class="styletext">
+					<h2 class="infoActua">Fourth Panel</h2>
+					<p class="white-text infoActua">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni iure error asperiores expedita odit aut consequatur
+						excepturi dignissimos, tempora itaque nam culpa quasi possimus odio vitae voluptatibus sunt iusto maiores!</p>
+				</div>
+			</div>
+		</div>
 
 
 		<!-- Partie contenant la barre d'alerte -->
 
 		<div class="barre_alert">
 			<div class="event_alert">
-
+				<img src="concert.png" class="testImg">
 			</div>
 			<div class="event_alert">
-
+				<img src="concert.png" class="testImg">
 			</div>
 			<div class="event_alert">
-
+				<img src="concert.png" class="testImg">
 			</div>
-
 
 		</div>
 
@@ -78,54 +86,60 @@
 
 	<section>
 
-		<!-- Partie du milieu de la page -->
-
-
-		<div class="col s12 m3">
-			<!-- Commentaires Facebook -->
-			<div>
-				<i class="fab fa-facebook"></i>
-
-
-				<br>
-
+		<div class="gridContainer">
+			<div class="cell">
+				<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam vel atque debitis quia provident ab obcaecati mollitia
+					nulla sequi explicabo!</p>
 			</div>
-
-			<!-- Abonnement newsletter -->
-			<div>
-
-				<h2>S'abonner à la newsletter</h2>
-
-				<div>
-					<br>
-					<a id="sabonner" class="waves-effect waves-light btn modal-trigger boutonColor" href="#modal_abo">S'abonner</a>
-				</div>
-				<div>
-					<br>
-					<a id="sedesabonner" class="waves-effect waves-light btn modal-trigger boutonColor" href="#modal_abo">Se désabonner</a>
-				</div>
-
-
+			<div class="cell">
+				<img src="piscine.jpg" class="testImg">
 			</div>
+			<div class="cell">
 
-
+				<img src="piscine.jpg" class="testImg">
+			</div>
+			<div class="cell">
+				<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam vel atque debitis quia provident ab obcaecati mollitia
+					nulla sequi explicabo!</p>
+			</div>
 		</div>
 
+		<div class="rightContainer">
+			<div class="fb">
+			</div>
+			<div class="news">
+				<h5>Newsletters</h5>
+				<div class="input-field size">
+					<input id="email" type="email" name="newsletter" required class="validate">
+					<label class="size" for="email">E-mail</label>
+				</div>
+				<div class="boutonNew">
+					<div>
+						<a id="sabonner" data-news="abonner" class=" ajaxBtn waves-effect waves-light btn  boutonColor">S'abonner</a>
+					</div>
+					<div>
+						<a id="sedesabonner" data-news="desabonner" class="ajaxBtn waves-effect waves-light btn  boutonColor">désabonner</a>
+					</div>
+				</div>
+			</div>
 		</div>
+		<!-- Abonnement newsletter -->
 	</section>
 
 
 
 	<?php
-		include 'footer.php';
-	?>
+include 'footer.php';
+?>
 
 
 
 
 
 		<!-- Modal -->
-		<div id="modal_abo" class="modal modal-fixed-footer">
+		<!-- href="#modal_abo"
+		modal-trigger -->
+		<!-- <div id="modal_abo" class="modal modal-fixed-footer">
 			<div class="modal-content">
 				<h4>
 					<strong>
@@ -140,11 +154,11 @@
 					<div>
 						<br>
 						<button id="desabo" class="btn waves-effect waves-light boutonColor ajaxBtn modal-action modal-close" name="desabonnement"
-						  type="button" data-news="desabonner">Se désabonner</button>
+						    type="button" data-news="desabonner">Se désabonner</button>
 					</div>
 					<div>
 						<button id="abo" class="btn waves-effect waves-light boutonColor ajaxBtn modal-action modal-close" type="button" name="abonnement"
-						  data-news="abonner">S'abonner</button>
+						    data-news="abonner">S'abonner</button>
 					</div>
 				</form>
 			</div>
@@ -152,7 +166,7 @@
 				<a href="#!" class="modal-action modal-close waves-effect waves-red btn-flat ">Annuler</a>
 			</div>
 
-		</div>
+		</div> -->
 
 		<script src="dist/js/vue.js"></script>
 		<script src="dist/js/jquery.js"></script>
@@ -161,7 +175,7 @@
 		<script src="dist/js/navfoot.js"></script>
 		<script src="dist/js/swipe.js"></script>
 		<script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl"
-		  crossorigin="anonymous"></script>
+		    crossorigin="anonymous"></script>
 		<script src="dist/js/main.js"></script>
 </body>
 
