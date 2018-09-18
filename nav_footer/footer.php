cc6ff7107cc725cf9b38014395c126fc6325eca6
thomas <onixyros@gmail.com>
2018-09-18 12:29:45 +0200

    added link to admin page in footer

--- nav_footer/footer.php
+++ nav_footer/footer.php
@@ -25,7 +25,7 @@
         <div class="footer-copyright">
           <div class="container">
           © 2018 Copyright Text
-          <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+          <a class="grey-text text-lighten-4 right" href="admin/admin.php">Admin</a>
           </div>
         </div>
       </footer>
