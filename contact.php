7d918c7dac188e497d6a18ae7b00f77f2e0fafb4
mikael <mikaelgueguen1@gmail.com>
2018-09-19 16:54:29 +0200

    new contact

--- contact.php
+++ contact.php
@@ -28,60 +28,77 @@
 <div class="container">
     <div class="row">
         <div class="col s12 m4 l8">
-            <div id="carte">
-            <iframe class="mapa" src="https://www.openstreetmap.org/export/embed.html?bbox=1.8257045745849612%2C43.934417643656786%2C1.8407034873962405%2C43.94308581326467&amp;layer=mapnik&amp;marker=43.93875961206273%2C1.8332147598266602" ></iframe><br/><small><a href="https://www.openstreetmap.org/?mlat=43.9388&amp;mlon=1.8332#map=16/43.9388/1.8332">Agrandir le plan</a></small>         
-            </div>
+            <iframe class="mapa" src="https://www.openstreetmap.org/export/embed.html?bbox=1.8257045745849612%2C43.934417643656786%2C1.8407034873962405%2C43.94308581326467&amp;layer=mapnik&amp;marker=43.93875961206273%2C1.8332147598266602" ></iframe>        
         </div>
-
-        <div class="col s12 m4 l4 valign-wrapper flow-text  amber lighten-2 z-depth-3"style="margin-top: 2%;">
-            <div class="card-content white-text">
-            <p>La canal, St. Jérome
-            Castelnau-De-Montmiral, Midi-Pyrenees, France
-            @colo.co.81
-            Appeler 05 63 57 42 33</p>
-            </div>
-        </div>
-
-        <form>
 
         <div class="col s12 m4 l4">
+            <div class="card-panel">
+                <span class="white-text">La Canal, St. Jérôme, 81140 Castelnau-de-Montmiral</span>
+                <p class="white-text">05 63 57 42 33</p>
+                <br>
+                <img class="responsive-img" src="img/index.jpeg"> 
+            </div>
+        </div>
+    </div>
+  
+
+    <form>
+        <div class="row">
+            <div class="col s12 white-text">
+                <h4><p>Formulaire de contact</p></h4>
+            </div>
+        </div>
+        <div class="row">
             <div class="input-field col s12">
+            <i class="material-icons prefix conticone">perm_identity</i>
                 <input id="nom" type="text" name="nom" class="validate" required>
                 <label for="nom">Nom</label>
             </div>
-
+        </div>
+        <div class="row">
             <div class="input-field col s12">
+            <i class="material-icons prefix conticone">person</i>
                 <input  id="prenom" name="prenom" type="text" class="validate" required>
                 <label for="prenom">Prénom</label>
             </div>
-
+        </div>
+        <div class="row">
             <div class="input-field col s12">
-                <input id="num" type="text" name="num" class="validate" required>
+            <i class="material-icons prefix conticone">phone</i>
+                <input id="num" type="text" name="num" class="validate">
                 <label for="num">Téléphone</label>
             </div>
-
+        </div>
+        <div class="row">
             <div class="input-field col s12">
+            <i class="material-icons prefix conticone">email</i>
                 <input id="email" type="email" name="email" class="validate" required>
                 <label for="email">Email</label>
             </div>
-
-           <div class="input-field col s12">
-            <textarea id="textarea2" class="materialize-textarea" data-length="120"></textarea>
-            <label for="textarea2">Message</label>
-          </div>
-
-            <button class="btn waves-effect waves-light" type="submit" name="action" style="margin-left: 32%;">Valider
-                <i class="material-icons right"></i>
-            </button>
         </div>
-        </form>
-    </div>
+        <div class="row">
+           <div class="input-field col s12">
+           <i class="material-icons prefix conticone">mode_edit</i>
+                <textarea id="icon_prefix2" class="materialize-textarea" data-length="120" required></textarea>
+                <label for="icon_prefix2">Message</label>
+            </div>
+        </div>
+        <div class="row">
+           <div class="col s12 center-align">
+                <button class="btn waves-effect waves-light" type="submit" name="action">Envoyer
+                <i class="material-icons right fleche">send</i>
+                </button>
+            </div>
+        </div>
+    </form>
+
 
         <div>
             <h3>Les services aux alentours</h3>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excep
             teur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
+        <br>
 </div>
 
 <?php 
