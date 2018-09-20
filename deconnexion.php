c189cf433b17bbc5296a3808ef895f421bf3b28a
JOHANNA <joana.jojo@hotmail.fr>
2018-09-20 09:22:59 +0200

    correction deconnexion

--- deconnexion.php
+++ deconnexion.php
@@ -4,10 +4,9 @@ include 'connexion/connexion_bdd.php';
 
 session_start();
 session_destroy();
-setcookie('e_mail', '', -1);
 
-if(isset($_COOKIE['e_mail'])) {
-	header ('Location: index.php');
-}
+
+header ('Location: index.php');
+
 
 ?>
