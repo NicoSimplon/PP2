8453098bff51d56dc6334757c8032ecffcc85565
nicolas <marty.nicolas11@laposte.net>
2018-09-20 10:39:06 +0200

    correction bug page accueil

--- index.php
+++ index.php
@@ -328,7 +328,7 @@ c4 -222 10 -787 14 -1257z m-417 29 l1 -325 -28 55 c-80 154 -131 373 -138
 					ORDER BY date_event asc
 					limit 3;");
 
-
+				$stockAlert = "";
 				while ($recupInfoAlert = pg_fetch_array($alertDyn)) {
 					$fin = '';
 					$debut = '';
