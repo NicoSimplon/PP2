c2883229e5a8d62b75ac90d4e3bd312875c5e7d9
thomas <onixyros@gmail.com>
2018-09-24 15:31:27 +0200

    liens mis en place, fil conducteur mis en place

--- js/navfoot.js
+++ js/navfoot.js
@@ -50,3 +50,35 @@ $(document).ready(function() {
       $(".petitAngre2,.petitAngre1", this).addClass("activeRotate");
     });
 });
+
+//Surligner les liens actifs
+var lien_actif = window.location.pathname;
+
+switch(lien_actif) {
+  case '/index.php':
+    $("#lien_accueil").css("color", "#577866");
+    break;
+
+  case '/evenement.php':
+    $("#lien_agenda, #engre_agenda1, #engre_agenda2").css("color", "#577866");
+    break;
+
+  case '/qui_sommes_nous.php':
+    $("#lien_apropos, #engre_apropos1, #engre_apropos2").css("color", "#577866");
+    break;
+
+  case '/contact.php':
+    $("#lien_contact, #engre_contact1, #engre_contact2").css("color", "#577866");
+    break;
+
+  case '/livre_dor.php':
+    $("#lien_commentaire, #engre_commentaire1, #engre_commentaire2").css("color", "#577866");
+    break;
+
+  case '/galerie.php':
+    $("#lien_galerie, #engre_galerie1, #engre_galerie2").css("color", "#577866");
+    break;
+
+  case '/propresse.php':
+    $("#lien_infopresse, #engre_infopresse1, #engre_infopresse2").css("color", "#577866");
+}
