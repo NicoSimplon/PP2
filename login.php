2a7a0ed9454d3dfaab57c72cee9bc3a570a64755
nicolas <marty.nicolas11@laposte.net>
2018-10-02 09:15:47 +0200

    correction nom de page

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 <html>
 
 <head>
-	<title>Accueil</title>
+	<title>Se connecter</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
